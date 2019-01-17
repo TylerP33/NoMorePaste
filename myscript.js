545bfdd97098e3c1f28666836e6760082c7cc86b
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2019-01-17 10:11:43 -0600

    Add clickbox capability to aim for certain highlight only

--- myscript.js
+++ myscript.js
@@ -1,7 +1,21 @@
-chrome.storage.local.get('color',function(block){
-	var color = JSON.parse(block.color)
-	$('code').css('background-color', color)
-	console.log(color)
-});
+function getColor(key, callback) {
+	if (key !== null) {
+		chrome.storage.local.get(key ,function (obj){
+			// $('.pln').css('background-color', block)
+			callback(obj)
+		});
+	}
+}
+
+
+getColor('color', function(color){
+	$('.kwd').css('background-color', color.color)
+})
+
+
+
+
+
+
 
 
--- popup.js
+++ popup.js
@@ -1,16 +1,23 @@
-window.onload=function(){grabData()}
+window.onload=function(){grabData(), changeHandler()}
 
 function grabData() {
-	let color = document.getElementById('test')
-	chrome.storage.local.set({
-    		'color': color.style.backgroundColor
-		}, function () {
-    		chrome.tabs.executeScript({
-        	file: "myscript.js"
-    	});
-	});
-
+	let spans = document.getElementsByTagName('span')
+	for (let i = 0; i < spans.length; i++){
+		spans[i].addEventListener("click", function(e) {
+			let color = e.target.style.backgroundColor
+				chrome.storage.local.set({
+    				'color': color
+				}, function () {
+    				chrome.tabs.executeScript({
+        			file: "myscript.js"
+    			});
+			});
+		});
+	}
 }
 
 
-
+function changeHandler(){
+     var checkedList = document.querySelectorAll("input[name^='item[']:checked")
+     console.log(checkedList)
+}
